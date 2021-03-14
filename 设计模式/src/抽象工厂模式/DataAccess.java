36bfa996866f2335cf1e7f225f3076735d1f6ba9
李俊波 <1632074768@qq.com>
2021-03-14 19:46:31 +0800

    first git

--- 设计模式/src/抽象工厂模式/DataAccess.java
+++ 设计模式/src/抽象工厂模式/DataAccess.java
@@ -1,11 +1,26 @@
 package 抽象工厂模式;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.IDN;
+import java.util.Properties;
 
 public class DataAccess {
+
     private static  String AssemblyName = "抽象工厂模式";
-    private static String db = "Access";
+    private static String db = "";
     //private static String db = "Sqlserver";
+
+    static {
+        Properties prop = new Properties();
+        InputStream is= DataAccess.class.getClassLoader().getResourceAsStream("db.properties");
+        try {
+            prop.load(is);
+            db = prop.getProperty("db");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static  IUser createUser() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         String className = AssemblyName + "." + db + "User";
